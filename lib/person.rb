d47b2f847ce02fb886cce458b4344cb35419b313
SpencerBlum <spencerblum@gmail.com>
2020-02-19 15:46:07 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+    
+    def job=(value)
+        @job = value
+    end
+
+    def job
+        @job
+        end
+
+    def name=(value)
+        @name = value
+    end
+
+    def name
+    @name
+    end
+
+
+end
